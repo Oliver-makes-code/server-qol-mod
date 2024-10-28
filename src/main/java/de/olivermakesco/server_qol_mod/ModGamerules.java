018883bd39b02c734c70ffc0612add3250a85d3d
Oliver-makes-code <oliver@olivermakesco.de>
2024-10-28 16:16:01 -0500

    Tags for stackables

--- src/main/java/de/olivermakesco/server_qol_mod/ModGamerules.java
+++ src/main/java/de/olivermakesco/server_qol_mod/ModGamerules.java
@@ -5,7 +5,7 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.level.GameRules;
 
 public class ModGamerules {
-    public static final GameRules.Key<GameRules.BooleanValue> STACKABLE_ITEMS = createBoolean("qol:16StackableItems", false);
+    public static final GameRules.Key<GameRules.BooleanValue> STACKABLE_ITEMS = createBoolean("qol:StackableItems", false);
     public static final GameRules.Key<GameRules.BooleanValue> CREEPERS_DESTROY_BLOCKS = createBoolean("qol:creepersDestroyBlocks", true);
     public static final GameRules.Key<GameRules.BooleanValue> ENDERMEN_PICK_UP_BLOCKS = createBoolean("qol:endermenPickUpBlocks", true);
     public static final GameRules.Key<GameRules.BooleanValue> PER_PLAYER_KEEP_INVENTORY = createBoolean("qol:perPlayerKeepInventory", false);
@@ -18,7 +18,9 @@ public class ModGamerules {
 
     public static boolean getBoolean(GameRules.Key<GameRules.BooleanValue> rule) {
         if (ServerQolMod.serverInstance == null)
-                return false;
+            return false;
+        if (ServerQolMod.serverInstance.overworld() == null)
+            return false;
         return ServerQolMod.serverInstance.getGameRules().getBoolean(rule);
     }
 
--- src/main/java/de/olivermakesco/server_qol_mod/ServerQolMod.java
+++ src/main/java/de/olivermakesco/server_qol_mod/ServerQolMod.java
@@ -6,8 +6,10 @@ import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +26,10 @@ public class ServerQolMod implements ModInitializer {
             .copyOnDeath()
             .buildAndRegister(rl("player_config"));
 
+    public static final TagKey<Item> SIXTEEN_STACKABLES = TagKey.create(Registries.ITEM, rl("sixteen_stackables"));
+    public static final TagKey<Item> THIRTY_TWO_STACKABLES = TagKey.create(Registries.ITEM, rl("thirty_two_stackables"));
+    public static final TagKey<Item> SIXTY_FOUR_STACKABLES = TagKey.create(Registries.ITEM, rl("sixty_four_stackables"));
+
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register(ModCommands::register);
@@ -37,15 +43,14 @@ public class ServerQolMod implements ModInitializer {
             if (!ModGamerules.getBoolean(ModGamerules.STACKABLE_ITEMS))
                 return;
 
-            if (
-                    stack.is(Items.POTION) ||
-                    stack.is(Items.LINGERING_POTION) ||
-                    stack.is(Items.SPLASH_POTION) ||
-                    (stack.getItem() instanceof BucketItem && stack.getItem() != Items.BUCKET) ||
-                    stack.getItem() instanceof SolidBucketItem ||
-                    stack.getItem().components().has(DataComponents.JUKEBOX_PLAYABLE)
-            )
+            if (stack.is(SIXTEEN_STACKABLES))
                 stack.set(DataComponents.MAX_STACK_SIZE, 16);
+
+            if (stack.is(THIRTY_TWO_STACKABLES))
+                stack.set(DataComponents.MAX_STACK_SIZE, 32);
+
+            if (stack.is(SIXTY_FOUR_STACKABLES))
+                stack.set(DataComponents.MAX_STACK_SIZE, 64);
         });
     }
 
